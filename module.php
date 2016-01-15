6798f20a88837abb7c6e17d7a80a980e32212d7d
ArronYR <yangyun4814@gmail.com>
2016-01-15 13:27:42 +0800

    完成导航nav

--- module.php
+++ module.php
@@ -3,6 +3,11 @@
  * 侧边栏组件、页面模块
  */
 if(!defined('EMLOG_ROOT')) {exit('error!');}
+
+global $CACHE;
+$navi_cache = $CACHE->readCache('navi');
+
+
 ?>
 
 <?php
--- modules/nav.php
+++ modules/nav.php
@@ -23,19 +23,37 @@ if(!defined('EMLOG_ROOT')) {exit('error!');}
                 <div class="search-results"></div>
             </div>
         </li>
-        <li class="bold active"><a href="about.html" class="waves-effect waves-teal">About</a></li>
-        <li class="bold"><a href="getting-started.html" class="waves-effect waves-teal">Getting Started</a></li>
-        <li class="no-padding">
-            <ul class="collapsible collapsible-accordion">
-                <li class="bold"><a class="collapsible-header  waves-effect waves-teal">CSS</a>
-                    <div class="collapsible-body" style="">
-                        <ul>
-                            <li><a href="color.html">Color</a></li>
-                            <li><a href="grid.html">Grid</a></li>
-                        </ul>
-                    </div>
+        <?php foreach ($navi_cache as $value):?>
+            <?php if($value['pid'] != 0){continue;}?>
+            <?php if($value['url'] == ROLE_ADMIN && (ROLE == ROLE_ADMIN || ROLE == ROLE_WRITER)):?>
+                <li class="bold"><a href="<?php echo BLOG_URL; ?>admin/" class="waves-effect waves-light">管理站点</a></li>
+                <li class="bold"><a href="<?php echo BLOG_URL; ?>admin/?action=logout" class="waves-effect waves-light">退出</a></li>
+            <?php continue; endif;
+                $newtab = $value['newtab'] == 'y' ? 'target="_blank"' : '';
+                $value['url'] = $value['isdefault'] == 'y' ? BLOG_URL . $value['url'] : trim($value['url'], '/');
+                $current_tab = BLOG_URL . trim(Dispatcher::setPath(), '/') == $value['url'] ? 'active' : '';
+            ?>
+            <?php if (!empty($value['childnavi'])) :?>
+                <li class="no-padding">
+                    <ul class="collapsible collapsible-accordion">
+                        <li class="bold">
+                            <a class="collapsible-header waves-effect waves-light"><?php echo $value['naviname']; ?></a>
+                            <div class="collapsible-body">
+                                <ul>
+                                    <?php foreach ($value['childnavi'] as $row):?>
+                                        <?php $newtab = $row['newtab'] == 'y' ? 'target="_blank"' : '';?>
+                                        <li><a class="waves-effect waves-light" href="<?php echo $row['url'];?>" <?php echo $newtab;?>><?php echo $row['naviname'];?></a></li>
+                                    <?php endforeach;?>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
                 </li>
-            </ul>
-        </li>
+            <?php else:?>
+                <li class="bold <?php echo $current_tab;?>">
+                    <a href="<?php echo $value['url']; ?>" <?php echo $newtab;?> class="waves-effect waves-light"><?php echo $value['naviname']; ?></a>
+                </li>
+            <?php endif;?>
+        <?php endforeach;?>
     </ul>
 </header>
